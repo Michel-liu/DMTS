5ccb555fbd50926a2adb56476d17c4b94b0cff8a
sunhanwu <sunhanwu@suns-imac.local>
2020-06-19 20:43:33 +0800

    create gui.py

---
+++ gui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+def CreateFrame():
+    top = Tk()
+    top.title("DMS测试")
+    top.geometry("1000x1000")
+    top.resizable(0,0)
+    return top
+top = CreateFrame()
+top.mainloop()
